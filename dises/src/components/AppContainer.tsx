ae1ee15ca0512137aab4bf381cd92b2e97cc8cb2
danwillett <115373509+danwillett@users.noreply.github.com>
2024-04-30 16:18:17 -0700

    I created a home page and added some initial content relating to the project description and images. I still need to add organization logos and contact information to the homepage

---
+++ dises/src/components/AppContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Grid, Container } from '@mui/material'
+
+import Home from './pages/Home'
+export default function AppContainer() {
+
+    return (
+        <Grid container>
+            <Home />
+        </Grid>
+    )
+}
